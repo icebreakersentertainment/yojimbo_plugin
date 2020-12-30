ead7f9e98a8bb1962d78312deaab535850996141
Jarrett Chisholm <jarrettchisholm@gmail.com>
2020-12-30 17:03:01 -0500

    - using conan for dependencies - cmake changes - removed libsodium and mbedtls submodules

--- yojimbo/compile.sh
+++ yojimbo/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-INCLUDES=-I../../libsodium/libsodium/src/libsodium/include\ -I../../mbedtls/mbedtls/include make config=release_x64
+INCLUDES=-I${1}\ -I${2} LDFLAGS=-L${3}\ -L${4} make config=release_x64
